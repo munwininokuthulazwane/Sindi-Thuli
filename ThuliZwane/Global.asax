9dca0269b9a6e37e78b54a53b6ad2492492d6427
Sindisiwe Mashini <SMashini@jhb.dvt.co.za>
2016-11-01 11:32:41 +0200

    am sorry for the changes

---
+++ ThuliZwane/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThuliZwane.MvcApplication" Language="C#" %>
---
+++ ThuliZwane/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThuliZwane.Models
+{
+    public class Login
+    {
+    }
+}
